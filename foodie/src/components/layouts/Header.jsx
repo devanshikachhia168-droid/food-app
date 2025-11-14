8814318089e2399315011e3da2920e29cec83284
devanshikachhia168-droid <devanshikachhia168@gmail.com>
2025-11-14 13:03:04 +0530

    created header.jsx

---
+++ foodie/src/components/layouts/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import flogo from '../../assets/images/logo.jpg'
+
+export default function Header() {
+  return (
+    <div>
+        <div className="header">
+            <div className="header-logo">
+    <img src={flogo} alt="" />
+            </div>
+            <div className="navbar">
+
+            </div>
+        </div>
+    </div>
+  )
+}
